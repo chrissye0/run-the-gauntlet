db18b45ccb935735dcd6c663cb6bda0fc27c4ed9
Chrissy Espeleta <120747293+chrissye0@users.noreply.github.com>
2026-09-03 22:31:17 -0400

    created player script, some renaming for consistency

--- run-the-gauntlet/Assets/Scripts/EnemyScripts/EnemyCombat.cs
+++ run-the-gauntlet/Assets/Scripts/EnemyScripts/EnemyCombat.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour
+public class EnemyCombat : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
