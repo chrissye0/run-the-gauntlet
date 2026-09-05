63e2a6d82c6c1f0e394836314ce2062e26ac450e
Chrissy Espeleta <120747293+chrissye0@users.noreply.github.com>
2026-09-04 23:15:33 -0400

    implemented nav mesh for enemies to approach the player but keep a distance

--- run-the-gauntlet/Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ run-the-gauntlet/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // reference to the player's Transform
+    public Transform player;
+    // reference to the NavMeshAgent component for pathfinding
+    private NavMeshAgent navMeshAgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // get and store the NavMeshAgent component attached to this object
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ff there's a reference to the player
+        if (player != null)
+        {
+            // set the enemy's destination to the player's current position
+            navMeshAgent.SetDestination(player.position);
+        }
     }
 }
