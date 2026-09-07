93676f7638ee14de2a6fec79c43b8dfbcfc55d9e
Chrissy Espeleta <120747293+chrissye0@users.noreply.github.com>
2026-09-06 22:14:25 -0400

    nearest enemy detection and destruction + player and enemy tags

--- run-the-gauntlet/Assets/Scripts/PlayerScripts/PlayerCombat.cs
+++ run-the-gauntlet/Assets/Scripts/PlayerScripts/PlayerCombat.cs
@@ -43,48 +43,94 @@ public class PlayerCombat : MonoBehaviour
         blockAction.performed += OnBlock;
     }
 
+    // returns the nearest enemy
+    GameObject ClosestEnemy()
+    {
+        // finds all existing game objects with tag "Enemy"
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        // for storing closest enemy and shortest distance
+        GameObject closestEnemy = null;
+        float shortestDistance = 10;
+        // loop through all found enemies
+        foreach (GameObject enemy in enemies)
+        {
+            // calculate distance
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            // see if it is the shortest and if it is within range
+            if (distance < shortestDistance && distance < 4)
+            {
+                // if so, set it to the closest enemy
+                shortestDistance = distance;
+                closestEnemy = enemy;
+            }
+        }
+        return closestEnemy;
+    }
+
+    // attack the nearest in bound enemy
+    void Punch()
+    {
+        if (ClosestEnemy() != null)
+        {
+            Debug.Log("Punch!!!");
+            Destroy(ClosestEnemy());
+        }
+    }
+
+    // in later iterations, also check for enemy armor and weaknesses in the below methods
+    // for instance, for uppercuts, check to see if the enemy does not have armor on its underside
+
     void OnLeftJab(InputAction.CallbackContext context)
     {
         Debug.Log("Left Jab");
+        Punch();
     }
 
     void OnLeftCross(InputAction.CallbackContext context)
     {
         Debug.Log("Left Cross");
+        Punch();
     }
 
     void OnLeftHook(InputAction.CallbackContext context)
     {
         Debug.Log("Left Hook");
+        Punch();
     }
 
     void OnLeftUppercut(InputAction.CallbackContext context)
     {
         Debug.Log("Left Uppercut");
+        Punch();
     }
 
     void OnRightJab(InputAction.CallbackContext context)
     {
         Debug.Log("Right Jab");
+        Punch();
     }
 
     void OnRightCross(InputAction.CallbackContext context)
     {
         Debug.Log("Right Cross");
+        Punch();
     }
 
     void OnRightHook(InputAction.CallbackContext context)
     {
         Debug.Log("Right Hook");
+        Punch();
     }
 
     void OnRightUppercut(InputAction.CallbackContext context)
     {
         Debug.Log("Right Uppercut");
+        Punch();
     }
 
     void OnBlock(InputAction.CallbackContext context)
     {
         Debug.Log("Block");
+        Punch();
     }
 }
