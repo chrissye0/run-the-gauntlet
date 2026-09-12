068ba508838946f884772d099807a0f111f6f54c
Chrissy Espeleta <120747293+chrissye0@users.noreply.github.com>
2026-09-11 23:11:35 -0400

    trying to add two hands to hardware, reference google doc notes, also added back text mesh pro asset files I guess I think idk mannnnnnn

--- imu-arduino-data/src/main.cpp
+++ imu-arduino-data/src/main.cpp
@@ -2,6 +2,24 @@
 #include "LSM6DS3.h"
 #include "Wire.h"
 
+// CHANGE LEFT OR RIGHT UPLOAD BASED ON ENV
+// UPDATE ENV BY CLICKING FOLDER ICON AT THE BOTTOM
+
+// #ifdef HAND_LEFT
+// #define HAND "LEFT"
+// #else
+// #define HAND "RIGHT"
+// #endif
+
+#ifdef HAND_LEFT
+#define HAND "LEFT"
+#elif defined(HAND_RIGHT)
+#define HAND "RIGHT"
+#else
+#define HAND "LEFT"
+#endif
+
+
 LSM6DS3 myIMU(I2C_MODE, 0x6A);
 
 void setup() {
@@ -13,7 +31,7 @@ void setup() {
         while (1);
     }
 
-    Serial.println("time,accelX,accelY,accelZ,gyroX,gyroY,gyroZ");
+    Serial.println("hand,time,accelX,accelY,accelZ,gyroX,gyroY,gyroZ");
 }
 
 void loop() {
@@ -24,6 +42,9 @@ void loop() {
     float gx = myIMU.readFloatGyroX();
     float gy = myIMU.readFloatGyroY();
     float gz = myIMU.readFloatGyroZ();
+
+    Serial.print(HAND);
+    Serial.print(",");
 
     Serial.print(millis());
     Serial.print(",");
